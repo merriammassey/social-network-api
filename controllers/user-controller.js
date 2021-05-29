4190ac34a5c8d0e2977d38a2ead628f7184e2555
Maria M <prov21_1@hotmail.com>
2021-05-29 14:10:45 -0700

    user methods

---
+++ controllers/user-controller.js
@@ -0,0 +1,59 @@
+const { User } = require("../models");
+
+//methods for getting, creating, deleting, and updating users
+const userController = {
+  getAllUser(req, res) {
+    User.find({})
+      .then((dbUser) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "That user doesn't exist" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  deleteUser({ params},res) {
+      User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+          if(!dbUserData) {
+              res.status(404).json({message: "That user doesn't exist"};
+              return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.status(404).json(err))
+    },
+};
+
+module.exports = userController;
